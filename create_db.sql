3aed2e2ff507bd8a8c37327d8d41ddedcd835d8d
Pasechnyk <pasechnyknastya@gmail.com>
2022-12-17 16:45:34 +0200

    Change Id format

--- create_db.sql
+++ create_db.sql
@@ -7,7 +7,7 @@ using University
 -- Departments
 create table Departments
 (    
-   ID int primary key identity(1, 1),
+   Id int primary key identity(1, 1),
    Financing money not null default(0) check(Financing >= 0),
    [Name] nvarchar(100) not null unique check([Name] <> ''),
    FacultyId int not null references Faculties(Id)
@@ -16,7 +16,7 @@ create table Departments
 -- Curators of groups
 create table GroupsCurators
 (
-   ID int primary key identity(1, 1),
+   Id int primary key identity(1, 1),
    CuratorId int not null references Curators(Id),
    GroupId int not null references Groups(Id)
 );
