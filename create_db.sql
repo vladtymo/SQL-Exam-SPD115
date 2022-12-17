bd5b1f15a15a932621ab06b4b6b01179b9ad623d
Bogdan <bogda026@gmail.com>
2022-12-17 15:30:42 +0200

    Create table Groups

--- create_db.sql
+++ create_db.sql
@@ -3,3 +3,11 @@
 create database TestDatabase;
 
 use TestDatabase;
+
+create table Groups
+(
+	Id int primary key identity(1,1),
+	Name nvarchar(10) not null check(Name <> '') unique,
+	Year int not null check(Year >= 1 and Year <= 5),
+	DepartmentId int not null references Departments(Id)
+);
