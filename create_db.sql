f9872cad849ae2b55c0bf33d9e24fee2fe3e541b
mostapchuk06 <101934840+mostapchuk06@users.noreply.github.com>
2022-12-17 16:14:11 +0200

    Created Curators and Lectures tables

--- create_db.sql
+++ create_db.sql
@@ -3,3 +3,17 @@
 create database TestDatabase;
 
 use TestDatabase;
+
+create Table Curators
+(
+   Id int primary key identity(1,1) not null,
+   Name nvarchar(max) not null check(Name <> ''),
+   Surname nvarchar(max) not null check(Surname <> '')
+)
+create table Lectures
+(
+   Id int primary key identity(1,1) not null,
+   LectureRoom nvarchar(max) not null check(LectureRoom <> ''),
+   SubjectId int not null references Subjects(Id),
+   TeacherId int not null references Teachers(Id),
+)
