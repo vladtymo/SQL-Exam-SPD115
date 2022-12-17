b2719b495e2cfd8b5e6915962cf576c894ba8071
Bogdan <bogda026@gmail.com>
2022-12-17 16:58:10 +0200

    Update create_db.sql

--- create_db.sql
+++ create_db.sql
@@ -8,7 +8,7 @@ create table Groups
 (
 	Id int primary key identity(1,1),
 	Name nvarchar(10) not null check(Name <> '') unique,
-	Year int not null check(Year >= 1 and Year <= 5),
+	Year int not null check(Year between 1 and 5),
 	DepartmentId int not null references Departments(Id)
 );
 
