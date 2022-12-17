f7dac51cd020099a9145fd87e0fa4f32786b677d
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2022-12-17 14:40:42 +0200

    Add test scripts.

--- create_db.sql
+++ create_db.sql
@@ -1,1 +1,11 @@
 -- SQL script for creating the Database
+
+create database TestDatabase;
+
+use TestDatabase;
+
+create table TestTable 
+(
+	Id int primary key identity,
+	Name nvarchar(100) not null check(Name <> '')
+)
