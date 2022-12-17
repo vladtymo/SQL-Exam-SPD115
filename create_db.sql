af1bef4e5a07f101e8fab7df5c5b352cd8f2f274
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2022-12-17 17:39:27 +0200

    Fix errors

--- create_db.sql
+++ create_db.sql
@@ -1,46 +1,32 @@
 -- SQL script for creating the Database
 
 -- Create Departments and GroupCurators tables
-create database University
-use University
+create database University;
+go
+use University;
+go
 
 create table Subjects 
 (
 	Id int primary key identity(1, 1) not null,
 	[Name] nvarchar(100) not null unique check([Name] <> ''),
 );
-
+go
 create table Faculties
 (
-Id int primary key IDENTITY(1,1) NOT NULL,
-Financing money NOT NULL CHECK(Financing >= 0) DEFAULT(0),
-Name nvarchar(100) NOT NULL CHECK(Name <> '') UNIQUE
-)
-
+	Id int primary key IDENTITY(1,1) NOT NULL,
+	Financing money NOT NULL CHECK(Financing >= 0) DEFAULT(0),
+	Name nvarchar(100) NOT NULL CHECK(Name <> '') UNIQUE
+);
+go
 create table Teachers
 (
-Id int primary key IDENTITY(1,1) NOT NULL,
-Name nvarchar(max) NOT NULL CHECK(Name <> ''),
-Salary money NOT NULL CHECK(Salary > 0),
-Surname nvarchar(max) NOT NULL CHECK(Surname <> '')
-)
-
-create table Groups
-(
-	Id int primary key identity(1,1),
-	Name nvarchar(10) not null check(Name <> '') unique,
-	Year int not null check(Year between 1 and 5),
-	DepartmentId int not null references Departments(Id)
+	Id int primary key IDENTITY(1,1) NOT NULL,
+	Name nvarchar(max) NOT NULL CHECK(Name <> ''),
+	Salary money NOT NULL CHECK(Salary > 0),
+	Surname nvarchar(max) NOT NULL CHECK(Surname <> '')
 );
-
-create table GroupsLectures
-(
-	Id int primary key identity(1,1),
-	GroupId int not null references Groups(Id),
-	LectureId int not null references Lectures(Id),
-);
-
--- Departments
+go
 create table Departments
 (    
    Id int primary key identity(1, 1),
@@ -48,25 +34,40 @@ create table Departments
    [Name] nvarchar(100) not null unique check([Name] <> ''),
    FacultyId int not null references Faculties(Id)
 );
-
--- Curators of groups
-create table GroupsCurators
+go
+create table Groups
 (
-   Id int primary key identity(1, 1),
-   CuratorId int not null references Curators(Id),
-   GroupId int not null references Groups(Id)
+	Id int primary key identity(1,1),
+	Name nvarchar(10) not null check(Name <> '') unique,
+	Year int not null check(Year between 1 and 5),
+	DepartmentId int not null references Departments(Id)
 );
-
-create Table Curators
-(
-   Id int primary key identity(1,1) not null,
-   Name nvarchar(max) not null check(Name <> ''),
-   Surname nvarchar(max) not null check(Surname <> '')
-)
+go
 create table Lectures
 (
    Id int primary key identity(1,1) not null,
    LectureRoom nvarchar(max) not null check(LectureRoom <> ''),
    SubjectId int not null references Subjects(Id),
    TeacherId int not null references Teachers(Id),
+);
+go
+create table GroupsLectures
+(
+	Id int primary key identity(1,1),
+	GroupId int not null references Groups(Id),
+	LectureId int not null references Lectures(Id),
+);
+go
+create Table Curators
+(
+   Id int primary key identity(1,1) not null,
+   Name nvarchar(max) not null check(Name <> ''),
+   Surname nvarchar(max) not null check(Surname <> '')
 )
+go
+create table GroupsCurators
+(
+   Id int primary key identity(1, 1),
+   CuratorId int not null references Curators(Id),
+   GroupId int not null references Groups(Id)
+);
--- insert_db.sql
+++ insert_db.sql
@@ -1,5 +1,8 @@
 -- SQL script for inserting data to the Database
 
+use University;
+go
+
 insert into Subjects (Name) values ( 'Computer Science');
 insert into Subjects (Name) values ('Information Systems');
 insert into Subjects (Name) values ( 'Chemical Engineering');
@@ -10,6 +13,7 @@ insert into Subjects (Name) values ( 'Physics');
 insert into Subjects (Name) values ( 'Physical Education');
 insert into Subjects (Name) values ( 'Biology');
 insert into Subjects (Name) values ('Geography');
+go
 
 insert into Teachers (Name, Salary, Surname) values ('Mallissa', 51, 'Whicher');
 insert into Teachers (Name, Salary, Surname) values ('Eleanora', 31, 'Roderigo');
@@ -31,6 +35,7 @@ insert into Teachers (Name, Salary, Surname) values ('Ursala', 58, 'Tejada');
 insert into Teachers (Name, Salary, Surname) values ('Consalve', 89, 'Alvarez');
 insert into Teachers (Name, Salary, Surname) values ('Elena', 70, 'Verne');
 insert into Teachers (Name, Salary, Surname) values ('Ivan', 32, 'Granleese');
+go
 
 insert into Faculties (financing, name) values ('$1119.03', 'Dabtype');
 insert into Faculties (financing, name) values ('$2948.43', 'Yata');
@@ -42,6 +47,7 @@ insert into Faculties (financing, name) values ('$2614.03', 'Voolia');
 insert into Faculties (financing, name) values ('$5938.83', 'Yombu');
 insert into Faculties (financing, name) values ('$1916.48', 'Tagtune');
 insert into Faculties (financing, name) values ('$8698.44', 'Wikido');
+go
 
 insert into Curators (Name, Surname) values ('Liam', 'Vankov');
 insert into Curators (Name, Surname) values ('Olivia', 'Del Checolo');
@@ -50,10 +56,11 @@ insert into Curators (Name, Surname) values ('Alma', 'Tarplee');
 insert into Curators (Name, Surname) values ('Ollie', 'Oakey');
 insert into Curators (Name, Surname) values ('Irv', 'Halfhyde');
 insert into Curators (Name, Surname) values ('Lettie', 'Andrieux');
+go
 
 insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('ShopRite Day Calm', 8, 12);
 insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('NuLev', 7, 11);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Levothyroxine Sodium', 11, 2);
+insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Levothyroxine Sodium', 10, 2);
 insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Pure petroleum', 6, 14);
 insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Alba Botanica Natural Even Advanced Sea Moss Moisturizer SPF15', 10, 12);
 insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Hydrocortisone', 9, 12);
@@ -62,55 +69,11 @@ insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Carbidopa and 
 insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Secret Clinical Invisible', 4, 4);
 insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Docetaxel', 9, 7);
 insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Potassium Chloride', 4, 4);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('ziprasidone hydrochloride', 11, 12);
+insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('ziprasidone hydrochloride', 4, 12);
 insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Venlafaxine Hydrochloride', 1, 1);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Pleo Lat', 11, 12);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Nystatin', 12, 10);
-
-insert into Groups (Name, Year, DepartmentId) values ('SPD-115', 4, 16);
-insert into Groups (Name, Year, DepartmentId) values ('SBU-331', 3, 14);
-insert into Groups (Name, Year, DepartmentId) values ('SPU-123', 4, 19);
-insert into Groups (Name, Year, DepartmentId) values ('PV-125', 5, 4);
-insert into Groups (Name, Year, DepartmentId) values ('PD-221', 1, 6);
-insert into Groups (Name, Year, DepartmentId) values ('VPD-121', 2, 1);
-insert into Groups (Name, Year, DepartmentId) values ('SPU-013', 1, 8);
-insert into Groups (Name, Year, DepartmentId) values ('VPD-011', 5, 18);
-insert into Groups (Name, Year, DepartmentId) values ('PD-014', 4, 16);
-insert into Groups (Name, Year, DepartmentId) values ('KI-21', 1, 5);
-insert into Groups (Name, Year, DepartmentId) values ('QA-25', 2, 19);
-insert into Groups (Name, Year, DepartmentId) values ('QA-22', 3, 4);
-insert into Groups (Name, Year, DepartmentId) values ('KFA-212', 2, 7);
-insert into Groups (Name, Year, DepartmentId) values ('LCA-117', 4, 18);
-insert into Groups (Name, Year, DepartmentId) values ('SAP-567', 4, 3);
-insert into Groups (Name, Year, DepartmentId) values ('USP-007', 4, 15);
-insert into Groups (Name, Year, DepartmentId) values ('ROI-097', 5, 9);
-insert into Groups (Name, Year, DepartmentId) values ('BU-228', 2, 20);
-insert into Groups (Name, Year, DepartmentId) values ('JEI-113', 3, 5);
-insert into Groups (Name, Year, DepartmentId) values ('KOI-232', 4, 17);
-
-insert into GroupsLectures (GroupId, LectureId) values (5, 20);
-insert into GroupsLectures (GroupId, LectureId) values (16, 13);
-insert into GroupsLectures (GroupId, LectureId) values (17, 15);
-insert into GroupsLectures (GroupId, LectureId) values (9, 14);
-insert into GroupsLectures (GroupId, LectureId) values (19, 13);
-insert into GroupsLectures (GroupId, LectureId) values (9, 12);
-insert into GroupsLectures (GroupId, LectureId) values (19, 12);
-insert into GroupsLectures (GroupId, LectureId) values (3, 14);
-insert into GroupsLectures (GroupId, LectureId) values (15, 18);
-insert into GroupsLectures (GroupId, LectureId) values (17, 15);
-insert into GroupsLectures (GroupId, LectureId) values (5, 13);
-insert into GroupsLectures (GroupId, LectureId) values (5, 1);
-insert into GroupsLectures (GroupId, LectureId) values (2, 4);
-insert into GroupsLectures (GroupId, LectureId) values (16, 1);
-insert into GroupsLectures (GroupId, LectureId) values (11, 10);
-insert into GroupsLectures (GroupId, LectureId) values (9, 10);
-insert into GroupsLectures (GroupId, LectureId) values (7, 20);
-insert into GroupsLectures (GroupId, LectureId) values (18, 11);
-insert into GroupsLectures (GroupId, LectureId) values (13, 1);
-insert into GroupsLectures (GroupId, LectureId) values (19, 7);
-
--- Inserting values to Department and GroupCurators tables
-using University
+insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Pleo Lat', 1, 12);
+insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Nystatin', 2, 10);
+go
 
 insert into Departments
 values (8000, 'Graphic Design', 1),
@@ -127,8 +90,52 @@ values (8000, 'Graphic Design', 1),
 	   (7400, 'Project Management', 6),
 	   (6900, 'Information Security', 6),
 	   (5800, 'Data Management', 6)
+go
 
--- insert data
+insert into Groups (Name, Year, DepartmentId) values ('SPD-115', 4, 12);
+insert into Groups (Name, Year, DepartmentId) values ('SBU-331', 3, 14);
+insert into Groups (Name, Year, DepartmentId) values ('SPU-123', 4, 1);
+insert into Groups (Name, Year, DepartmentId) values ('PV-125', 5, 4);
+insert into Groups (Name, Year, DepartmentId) values ('PD-221', 1, 6);
+insert into Groups (Name, Year, DepartmentId) values ('VPD-121', 2, 1);
+insert into Groups (Name, Year, DepartmentId) values ('SPU-013', 1, 8);
+insert into Groups (Name, Year, DepartmentId) values ('VPD-011', 5, 14);
+insert into Groups (Name, Year, DepartmentId) values ('PD-014', 4, 6);
+insert into Groups (Name, Year, DepartmentId) values ('KI-21', 1, 5);
+insert into Groups (Name, Year, DepartmentId) values ('QA-25', 2, 9);
+insert into Groups (Name, Year, DepartmentId) values ('QA-22', 3, 4);
+insert into Groups (Name, Year, DepartmentId) values ('KFA-212', 2, 7);
+insert into Groups (Name, Year, DepartmentId) values ('LCA-117', 4, 8);
+insert into Groups (Name, Year, DepartmentId) values ('SAP-567', 4, 3);
+insert into Groups (Name, Year, DepartmentId) values ('USP-007', 4, 5);
+insert into Groups (Name, Year, DepartmentId) values ('ROI-097', 5, 9);
+insert into Groups (Name, Year, DepartmentId) values ('BU-228', 2, 2);
+insert into Groups (Name, Year, DepartmentId) values ('JEI-113', 3, 5);
+insert into Groups (Name, Year, DepartmentId) values ('KOI-232', 4, 7);
+go
+
+insert into GroupsLectures (GroupId, LectureId) values (5, 15);
+insert into GroupsLectures (GroupId, LectureId) values (16, 13);
+insert into GroupsLectures (GroupId, LectureId) values (17, 15);
+insert into GroupsLectures (GroupId, LectureId) values (9, 14);
+insert into GroupsLectures (GroupId, LectureId) values (19, 13);
+insert into GroupsLectures (GroupId, LectureId) values (9, 12);
+insert into GroupsLectures (GroupId, LectureId) values (19, 12);
+insert into GroupsLectures (GroupId, LectureId) values (3, 14);
+insert into GroupsLectures (GroupId, LectureId) values (15, 8);
+insert into GroupsLectures (GroupId, LectureId) values (17, 15);
+insert into GroupsLectures (GroupId, LectureId) values (5, 13);
+insert into GroupsLectures (GroupId, LectureId) values (5, 1);
+insert into GroupsLectures (GroupId, LectureId) values (2, 4);
+insert into GroupsLectures (GroupId, LectureId) values (16, 1);
+insert into GroupsLectures (GroupId, LectureId) values (11, 10);
+insert into GroupsLectures (GroupId, LectureId) values (9, 10);
+insert into GroupsLectures (GroupId, LectureId) values (7, 2);
+insert into GroupsLectures (GroupId, LectureId) values (18, 11);
+insert into GroupsLectures (GroupId, LectureId) values (13, 1);
+insert into GroupsLectures (GroupId, LectureId) values (19, 7);
+go
+
 insert into GroupsCurators
 values (1, 3),
        (4, 5),
@@ -137,11 +144,12 @@ values (1, 3),
 	   (2, 2),
 	   (6, 7),
 	   (7, 1),
-	   (10, 9),
-	   (8, 6),
-	   (9, 10),
-	   (17, 13),
-	   (12, 18),
-	   (11, 14),
-	   (13, 19),
-	   (20, 11)
+	   (1, 9),
+	   (7, 6),
+	   (3, 10),
+	   (7, 13),
+	   (2, 18),
+	   (1, 14),
+	   (3, 19),
+	   (2, 11)
+go
