397a2a36c940b2cc101ac1980d82d284481db9a6
nevizir <smister0987@gmail.com>
2022-12-17 16:58:02 +0300

    create and instert
    create table Faculties and table Teachers
    insert table Faculties and table Teachers

--- create_db.sql
+++ create_db.sql
@@ -3,3 +3,18 @@
 create database BarberShopDb;
 
 use BarberShopDb;
+
+create table Faculties
+(
+Id int primary key IDENTITY(1,1) NOT NULL,
+Financing money NOT NULL CHECK(Financing > 0) DEFAULT(0),
+Name nvarchar(100) NOT NULL CHECK(Name <> '') UNIQUE
+)
+
+create table Teachers
+(
+Id int primary key IDENTITY(1,1) NOT NULL,
+Name nvarchar(max) NOT NULL CHECK(Name <> ''),
+Salary money NOT NULL CHECK(Salary > 0),
+Surname nvarchar(max) NOT NULL CHECK(Surname <> '')
+)
--- insert_db.sql
+++ insert_db.sql
@@ -1,3 +1,37 @@
 -- SQL script for inserting data to the Database
 
 -- create table _
+
+use BarberShopDb;
+
+insert into Teachers (Name, Salary, Surname) values ('Mallissa', 51, 'Whicher');
+insert into Teachers (Name, Salary, Surname) values ('Eleanora', 31, 'Roderigo');
+insert into Teachers (Name, Salary, Surname) values ('Royce', 51, 'Meffin');
+insert into Teachers (Name, Salary, Surname) values ('Codee', 85, 'Ivanilov');
+insert into Teachers (Name, Salary, Surname) values ('Ethyl', 58, 'Jerzyk');
+insert into Teachers (Name, Salary, Surname) values ('Frannie', 16, 'Damerell');
+insert into Teachers (Name, Salary, Surname) values ('Leonelle', 73, 'Meek');
+insert into Teachers (Name, Salary, Surname) values ('Anica', 14, 'Corry');
+insert into Teachers (Name, Salary, Surname) values ('Mame', 81, 'Mee');
+insert into Teachers (Name, Salary, Surname) values ('Adam', 70, 'Spruce');
+insert into Teachers (Name, Salary, Surname) values ('Ange', 87, 'Diprose');
+insert into Teachers (Name, Salary, Surname) values ('Ivy', 71, 'Leavry');
+insert into Teachers (Name, Salary, Surname) values ('Maurizio', 19, 'MacConchie');
+insert into Teachers (Name, Salary, Surname) values ('Lynnett', 69, 'Sommerville');
+insert into Teachers (Name, Salary, Surname) values ('Pate', 45, 'Purvis');
+insert into Teachers (Name, Salary, Surname) values ('Sari', 11, 'Rallings');
+insert into Teachers (Name, Salary, Surname) values ('Ursala', 58, 'Tejada');
+insert into Teachers (Name, Salary, Surname) values ('Consalve', 89, 'Alvarez');
+insert into Teachers (Name, Salary, Surname) values ('Elena', 70, 'Verne');
+insert into Teachers (Name, Salary, Surname) values ('Ivan', 32, 'Granleese');
+
+insert into Faculties (financing, name) values ('$1119.03', 'Dabtype');
+insert into Faculties (financing, name) values ('$2948.43', 'Yata');
+insert into Faculties (financing, name) values ('$2467.04', 'Chatterpoint');
+insert into Faculties (financing, name) values ('$9065.71', 'Yotz');
+insert into Faculties (financing, name) values ('$3924.59', 'Rhynoodle');
+insert into Faculties (financing, name) values ('$7752.62', 'Jabberstorm');
+insert into Faculties (financing, name) values ('$2614.03', 'Voolia');
+insert into Faculties (financing, name) values ('$5938.83', 'Yombu');
+insert into Faculties (financing, name) values ('$1916.48', 'Tagtune');
+insert into Faculties (financing, name) values ('$8698.44', 'Wikido');
