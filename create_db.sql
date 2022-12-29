13b6b9ec92addef489601b1f5a9038ae1e1cdd3c
mostapchuk06 <101934840+mostapchuk06@users.noreply.github.com>
2022-12-29 21:32:34 +0200

    Create BarberShop database

--- create_db.sql
+++ create_db.sql
@@ -1,73 +1,88 @@
--- SQL script for creating the Database
+create database BarberShop
+use BarberShop
 
--- Create Departments and GroupCurators tables
-create database University;
-go
-use University;
-go
-
-create table Subjects 
+create table Positions
 (
-	Id int primary key identity(1, 1) not null,
-	[Name] nvarchar(100) not null unique check([Name] <> ''),
+    Id int primary key identity(1,1),
+	Name nvarchar(10) not null check(Name <> '')
 );
-go
-create table Faculties
+create table [Services]
 (
-	Id int primary key IDENTITY(1,1) NOT NULL,
-	Financing money NOT NULL CHECK(Financing >= 0) DEFAULT(0),
-	Name nvarchar(100) NOT NULL CHECK(Name <> '') UNIQUE
+    Id int primary key identity(1,1),
+	Name nvarchar(10) not null check(Name <> ''),
+	Price money not null default(0),
+	Duration int not null
 );
-go
-create table Teachers
+create table Customers
 (
-	Id int primary key IDENTITY(1,1) NOT NULL,
-	Name nvarchar(max) NOT NULL CHECK(Name <> ''),
-	Salary money NOT NULL CHECK(Salary > 0),
-	Surname nvarchar(max) NOT NULL CHECK(Surname <> '')
+    Id int primary key identity(1,1),
+	Name nvarchar(10) not null check(Name <> ''),
+	Surname nvarchar(10) not null check(Surname <> ''),
+	Father nvarchar(10) not null check(Father <> ''),
+	Email nvarchar(100) not null check(Email <> '') unique,
 );
-go
-create table Departments
-(    
-   Id int primary key identity(1, 1),
-   Financing money not null default(0) check(Financing >= 0),
-   [Name] nvarchar(100) not null unique check([Name] <> ''),
-   FacultyId int not null references Faculties(Id)
-);
-go
-create table Groups
+create table Barbers
 (
 	Id int primary key identity(1,1),
-	Name nvarchar(10) not null check(Name <> '') unique,
-	Year int not null check(Year between 1 and 5),
-	DepartmentId int not null references Departments(Id)
+	Name nvarchar(10) not null check(Name <> ''),
+	Surname nvarchar(10) not null check(Surname <> ''),
+	Father nvarchar(10) not null check(Father <> ''),
+	Sex nvarchar(10) not null check(Sex <> ''),
+	Phone nvarchar(12) not null check(Phone <> '') unique,
+	Email nvarchar(100) not null check(Email <> '') unique,
+	BirthDate date not null check(BirthDate <> ''),
+	EmploymentDate date not null,
+	PositionId int not null references Positions(Id)
 );
-go
-create table Lectures
+create table Records
 (
-   Id int primary key identity(1,1) not null,
-   LectureRoom nvarchar(max) not null check(LectureRoom <> ''),
-   SubjectId int not null references Subjects(Id),
-   TeacherId int not null references Teachers(Id),
+    Id int primary key identity(1,1),
+	CustomerId int not null references Customers(Id),
+	BarbersId int not null references Barbers(Id),
+	[Date] date not null,
+	[Time] time not null
 );
-go
-create table GroupsLectures
+create table Schedule
 (
-	Id int primary key identity(1,1),
-	GroupId int not null references Groups(Id),
-	LectureId int not null references Lectures(Id),
+    Id int primary key identity(1,1),	
+	BarbersId int not null references Barbers(Id),
+	[Day] int not null,
+	[Start] time not null,
+	[End] time not null
 );
-go
-create Table Curators
+create table Feedbacks
 (
-   Id int primary key identity(1,1) not null,
-   Name nvarchar(max) not null check(Name <> ''),
-   Surname nvarchar(max) not null check(Surname <> '')
-)
-go
-create table GroupsCurators
+    Id int primary key identity(1,1),
+	Feedback nvarchar(max) not null,
+	CustomerId int not null references Customers(Id),
+	BarberId int not null references Barbers(Id),
+);
+create table EstimationTypes
 (
-   Id int primary key identity(1, 1),
-   CuratorId int not null references Curators(Id),
-   GroupId int not null references Groups(Id)
+    Id int primary key identity(1,1),
+	[Type] nvarchar(100) not null check([Type] <> ''),
+);
+create table Estimations
+(
+    Id int primary key identity(1,1),
+	CustomerId int not null references Customers(Id),
+	BarbersId int not null references Barbers(Id),
+	TypeId int not null references EstimationTypes(Id),
+);
+create table BarbersServices
+(
+    Id int primary key identity(1,1),
+	ServiceId int not null references [Services](Id),
+	BarberId int not null references Barbers(Id)
+);
+create table Archive
+(
+    Id int primary key identity(1,1),
+	CustomerId int not null references Customers(Id),
+	BarbersId int not null references Barbers(Id),
+	ServiceId int not null references [Services](Id),
+	[Date] date not null,
+	TotalPrice money not null default(0),
+	EstimationId int not null references Estimations(Id),
+	FeedbackId int not null references Feedbacks(Id)
 );
--- insert_db.sql
+++ insert_db.sql
@@ -1,155 +1,117 @@
 -- SQL script for inserting data to the Database
 
-use University;
-go
+use BarberShop;
 
-insert into Subjects (Name) values ( 'Computer Science');
-insert into Subjects (Name) values ('Information Systems');
-insert into Subjects (Name) values ( 'Chemical Engineering');
-insert into Subjects (Name) values ( 'Structural Engineering');
-insert into Subjects (Name) values ( 'Mechanical Engineering');
-insert into Subjects (Name) values ( 'Mathematics');
-insert into Subjects (Name) values ( 'Physics');
-insert into Subjects (Name) values ( 'Physical Education');
-insert into Subjects (Name) values ( 'Biology');
-insert into Subjects (Name) values ('Geography');
-go
+insert into Positions (Name) values ('cheaf');
+insert into Positions (Name) values ('junior');
+insert into Positions (Name) values ('senior');
 
-insert into Teachers (Name, Salary, Surname) values ('Mallissa', 51, 'Whicher');
-insert into Teachers (Name, Salary, Surname) values ('Eleanora', 31, 'Roderigo');
-insert into Teachers (Name, Salary, Surname) values ('Royce', 51, 'Meffin');
-insert into Teachers (Name, Salary, Surname) values ('Codee', 85, 'Ivanilov');
-insert into Teachers (Name, Salary, Surname) values ('Ethyl', 58, 'Jerzyk');
-insert into Teachers (Name, Salary, Surname) values ('Frannie', 16, 'Damerell');
-insert into Teachers (Name, Salary, Surname) values ('Leonelle', 73, 'Meek');
-insert into Teachers (Name, Salary, Surname) values ('Anica', 14, 'Corry');
-insert into Teachers (Name, Salary, Surname) values ('Mame', 81, 'Mee');
-insert into Teachers (Name, Salary, Surname) values ('Adam', 70, 'Spruce');
-insert into Teachers (Name, Salary, Surname) values ('Ange', 87, 'Diprose');
-insert into Teachers (Name, Salary, Surname) values ('Ivy', 71, 'Leavry');
-insert into Teachers (Name, Salary, Surname) values ('Maurizio', 19, 'MacConchie');
-insert into Teachers (Name, Salary, Surname) values ('Lynnett', 69, 'Sommerville');
-insert into Teachers (Name, Salary, Surname) values ('Pate', 45, 'Purvis');
-insert into Teachers (Name, Salary, Surname) values ('Sari', 11, 'Rallings');
-insert into Teachers (Name, Salary, Surname) values ('Ursala', 58, 'Tejada');
-insert into Teachers (Name, Salary, Surname) values ('Consalve', 89, 'Alvarez');
-insert into Teachers (Name, Salary, Surname) values ('Elena', 70, 'Verne');
-insert into Teachers (Name, Salary, Surname) values ('Ivan', 32, 'Granleese');
-go
+insert into [Services] (Name, Price, Duration) values ('sapien', '$1.14', '11:06');
+insert into [Services] (Name, Price, Duration) values ('amet', '$2.68', '11:11');
+insert into [Services] (Name, Price, Duration) values ('sociis', '$4.11', '11:13');
+insert into [Services] (Name, Price, Duration) values ('quam', '$1.06', '14:58');
+insert into [Services] (Name, Price, Duration) values ('quam', '$4.19', '11:41');
+insert into [Services] (Name, Price, Duration) values ('nisl', '$2.46', '11:06');
+insert into [Services] (Name, Price, Duration) values ('in', '$8.24', '11:09');
+insert into [Services] (Name, Price, Duration) values ('augue', '$2.90', '18:42');
+insert into [Services] (Name, Price, Duration) values ('ligula', '$6.17', '17:02');
+insert into [Services] (Name, Price, Duration) values ('ante', '$1.38', '19:13');
 
-insert into Faculties (financing, name) values ('$1119.03', 'Dabtype');
-insert into Faculties (financing, name) values ('$2948.43', 'Yata');
-insert into Faculties (financing, name) values ('$2467.04', 'Chatterpoint');
-insert into Faculties (financing, name) values ('$9065.71', 'Yotz');
-insert into Faculties (financing, name) values ('$3924.59', 'Rhynoodle');
-insert into Faculties (financing, name) values ('$7752.62', 'Jabberstorm');
-insert into Faculties (financing, name) values ('$2614.03', 'Voolia');
-insert into Faculties (financing, name) values ('$5938.83', 'Yombu');
-insert into Faculties (financing, name) values ('$1916.48', 'Tagtune');
-insert into Faculties (financing, name) values ('$8698.44', 'Wikido');
-go
+insert into Customers (Name, Surname, Father, Email) values ('Hester', 'Larmor', 'Kohrding', 'hkohrding0@hc360.com');
+insert into Customers (Name, Surname, Father, Email) values ('Marlo', 'Beller', 'Chicotti', 'mchicotti1@google.com.au');
+insert into Customers (Name, Surname, Father, Email) values ('Chanda', 'Witling', 'Waterstone', 'cwaterstone2@ed.gov');
+insert into Customers (Name, Surname, Father, Email) values ('Link', 'Bignell', 'Creddon', 'lcreddon3@java.com');
+insert into Customers (Name, Surname, Father, Email) values ('Giulietta', 'Renzullo', 'Tregunnah', 'gtregunnah4@stanford.edu');
+insert into Customers (Name, Surname, Father, Email) values ('Jewelle', 'Sodory', 'Pesik', 'jpesik5@vkontakte.ru');
+insert into Customers (Name, Surname, Father, Email) values ('Joelly', 'Paulet', 'Fitzsymons', 'jfitzsymons6@marriott.com');
+insert into Customers (Name, Surname, Father, Email) values ('Harriette', 'Slegg', 'Smallman', 'hsmallman7@cmu.edu');
+insert into Customers (Name, Surname, Father, Email) values ('Antonius', 'Jagger', 'Rodgerson', 'arodgerson8@sakura.ne.jp');
+insert into Customers (Name, Surname, Father, Email) values ('Blayne', 'Braiden', 'Gloy', 'bgloy9@wiley.com');
 
-insert into Curators (Name, Surname) values ('Liam', 'Vankov');
-insert into Curators (Name, Surname) values ('Olivia', 'Del Checolo');
-insert into Curators (Name, Surname) values ('Oliver', 'Gane');
-insert into Curators (Name, Surname) values ('Alma', 'Tarplee');
-insert into Curators (Name, Surname) values ('Ollie', 'Oakey');
-insert into Curators (Name, Surname) values ('Irv', 'Halfhyde');
-insert into Curators (Name, Surname) values ('Lettie', 'Andrieux');
-go
+insert into Barbers (Name, Surname, Father, Sex, Phone, Email, BirthDate, EmploymentDate, PositionId) values ('Michel', 'Maple', 'Madison', 'Male', '214-627-6877', 'mmadison0@edublogs.org', '26/7/1973', '24/3/2003', 1);
+insert into Barbers (Name, Surname, Father, Sex, Phone, Email, BirthDate, EmploymentDate, PositionId) values ('Meggie', 'Olczak', 'Coggill', 'Female', '761-637-8460', 'mcoggill1@multiply.com', '22/9/1989', '25/6/2022', 2);
+insert into Barbers (Name, Surname, Father, Sex, Phone, Email, BirthDate, EmploymentDate, PositionId) values ('Chris', 'Royl', 'Baggott', 'Female', '507-215-7519', 'cbaggott2@mapy.cz', '1/7/2000', '22/2/2005', 3);
+insert into Barbers (Name, Surname, Father, Sex, Phone, Email, BirthDate, EmploymentDate, PositionId) values ('Tuck', 'Ebhardt', 'Gentsch', 'Male', '951-645-7684', 'tgentsch3@list-manage.com', '6/6/2019', '22/8/2002', 3);
+insert into Barbers (Name, Surname, Father, Sex, Phone, Email, BirthDate, EmploymentDate, PositionId) values ('Fayina', 'Sheddan', 'Mackro', 'Female', '783-852-3796', 'fmackro4@amazonaws.com', '7/3/1964', '4/7/2013', 2);
+insert into Barbers (Name, Surname, Father, Sex, Phone, Email, BirthDate, EmploymentDate, PositionId) values ('Ernaline', 'Skipworth', 'Cruse', 'Female', '284-529-5699', 'ecruse5@instagram.com', '11/8/1973', '18/1/2008', 2);
+insert into Barbers (Name, Surname, Father, Sex, Phone, Email, BirthDate, EmploymentDate, PositionId) values ('Brigg', 'de''-Ancy Willis', 'Otton', 'Male', '287-300-5640', 'botton6@gmpg.org', '15/12/1993', '20/3/2021', 3);
+insert into Barbers (Name, Surname, Father, Sex, Phone, Email, BirthDate, EmploymentDate, PositionId) values ('Renault', 'McMinn', 'Petrowsky', 'Male', '956-339-5796', 'rpetrowsky7@webnode.com', '31/3/1993', '10/10/2019', 2);
+insert into Barbers (Name, Surname, Father, Sex, Phone, Email, BirthDate, EmploymentDate, PositionId) values ('Quintina', 'Elwell', 'Vassar', 'Female', '878-934-3735', 'qvassar8@biblegateway.com', '7/6/1949', '1/9/2019', 2);
+insert into Barbers (Name, Surname, Father, Sex, Phone, Email, BirthDate, EmploymentDate, PositionId) values ('Gerick', 'Faber', 'Aspital', 'Male', '504-223-7733', 'gaspital9@last.fm', '9/5/2021', '25/1/2002', 2);
 
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('ShopRite Day Calm', 8, 12);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('NuLev', 7, 11);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Levothyroxine Sodium', 10, 2);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Pure petroleum', 6, 14);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Alba Botanica Natural Even Advanced Sea Moss Moisturizer SPF15', 10, 12);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Hydrocortisone', 9, 12);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Venlafaxine Hydrochloride', 3, 8);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Carbidopa and Levodopa', 1, 2);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Secret Clinical Invisible', 4, 4);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Docetaxel', 9, 7);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Potassium Chloride', 4, 4);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('ziprasidone hydrochloride', 4, 12);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Venlafaxine Hydrochloride', 1, 1);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Pleo Lat', 1, 12);
-insert into Lectures (LectureRoom, SubjectId, TeacherId) values ('Nystatin', 2, 10);
-go
+insert into Records (CustomerId, BarbersId, [Date], [Time]) values (1, 1, '12/25/2022', '21:44');
+insert into Records (CustomerId, BarbersId, [Date], [Time]) values (2, 2, '12/29/2021', '1:42');
+insert into Records (CustomerId, BarbersId, [Date], [Time]) values (3, 3, '7/25/2022', '16:32');
+insert into Records (CustomerId, BarbersId, [Date], [Time]) values (4, 4, '8/2/2022', '10:03');
+insert into Records (CustomerId, BarbersId, [Date], [Time]) values (5, 5, '5/25/2022', '19:33');
+insert into Records (CustomerId, BarbersId, [Date], [Time]) values (6, 6, '4/12/2022', '12:50');
+insert into Records (CustomerId, BarbersId, [Date], [Time]) values (7, 7, '10/1/2022', '5:14');
+insert into Records (CustomerId, BarbersId, [Date], [Time]) values (8, 8, '5/12/2022', '17:33');
+insert into Records (CustomerId, BarbersId, [Date], [Time]) values (9, 9, '6/2/2022', '8:05');
+insert into Records (CustomerId, BarbersId, [Date], [Time]) values (10, 10, '9/17/2022', '5:13');
 
-insert into Departments
-values (8000, 'Graphic Design', 1),
-       (6200, 'Software Development', 2),
-	   (7800, 'Testing', 2),
-	   (9900, 'Cybersecurity', 3),
-	   (7500, 'Visual Design', 1),
-	   (7100, 'Systems Engeneering', 4),
-	   (8600, 'Network Engeneering', 4),
-	   (9500, 'Web Developing', 1),
-	   (6300, 'Database Administration', 3),
-	   (8500, 'Computer Architecture', 5),
-	   (8000, 'Computer Support', 5),
-	   (7400, 'Project Management', 6),
-	   (6900, 'Information Security', 6),
-	   (5800, 'Data Management', 6)
-go
+insert into Schedule (BarbersId, [Day], [Start], [End]) values (1, 1, '19:54', '7:40');
+insert into Schedule (BarbersId, [Day], [Start], [End]) values (2, 2, '13:13', '6:00');
+insert into Schedule (BarbersId, [Day], [Start], [End]) values (3, 3, '22:27', '9:36');
+insert into Schedule (BarbersId, [Day], [Start], [End]) values (4, 4, '15:21', '11:28');
+insert into Schedule (BarbersId, [Day], [Start], [End]) values (5, 5, '15:33', '22:25');
+insert into Schedule (BarbersId, [Day], [Start], [End]) values (6, 6, '10:53', '4:08');
+insert into Schedule (BarbersId, [Day], [Start], [End]) values (7, 7, '19:05', '4:33');
+insert into Schedule (BarbersId, [Day], [Start], [End]) values (8, 8, '16:44', '2:43');
+insert into Schedule (BarbersId, [Day], [Start], [End]) values (9, 9, '0:09', '7:45');
+insert into Schedule (BarbersId, [Day], [Start], [End]) values (10, 10, '10:08', '18:55');
 
-insert into Groups (Name, Year, DepartmentId) values ('SPD-115', 4, 12);
-insert into Groups (Name, Year, DepartmentId) values ('SBU-331', 3, 14);
-insert into Groups (Name, Year, DepartmentId) values ('SPU-123', 4, 1);
-insert into Groups (Name, Year, DepartmentId) values ('PV-125', 5, 4);
-insert into Groups (Name, Year, DepartmentId) values ('PD-221', 1, 6);
-insert into Groups (Name, Year, DepartmentId) values ('VPD-121', 2, 1);
-insert into Groups (Name, Year, DepartmentId) values ('SPU-013', 1, 8);
-insert into Groups (Name, Year, DepartmentId) values ('VPD-011', 5, 14);
-insert into Groups (Name, Year, DepartmentId) values ('PD-014', 4, 6);
-insert into Groups (Name, Year, DepartmentId) values ('KI-21', 1, 5);
-insert into Groups (Name, Year, DepartmentId) values ('QA-25', 2, 9);
-insert into Groups (Name, Year, DepartmentId) values ('QA-22', 3, 4);
-insert into Groups (Name, Year, DepartmentId) values ('KFA-212', 2, 7);
-insert into Groups (Name, Year, DepartmentId) values ('LCA-117', 4, 8);
-insert into Groups (Name, Year, DepartmentId) values ('SAP-567', 4, 3);
-insert into Groups (Name, Year, DepartmentId) values ('USP-007', 4, 5);
-insert into Groups (Name, Year, DepartmentId) values ('ROI-097', 5, 9);
-insert into Groups (Name, Year, DepartmentId) values ('BU-228', 2, 2);
-insert into Groups (Name, Year, DepartmentId) values ('JEI-113', 3, 5);
-insert into Groups (Name, Year, DepartmentId) values ('KOI-232', 4, 7);
-go
+insert into Feedbacks (Feedback, CustomerId, BarbersId) values ('curae nulla dapibus dolor vel', 1, 1);
+insert into Feedbacks (Feedback, CustomerId, BarbersId) values ('est lacinia nisi venenatis tristique fusce congue diam', 2, 2);
+insert into Feedbacks (Feedback, CustomerId, BarbersId) values ('morbi odio odio elementum eu interdum eu tincidunt in leo', 3, 3);
+insert into Feedbacks (Feedback, CustomerId, BarbersId) values ('orci eget orci vehicula condimentum curabitur', 4, 4);
+insert into Feedbacks (Feedback, CustomerId, BarbersId) values ('non mattis pulvinar nulla pede ullamcorper augue a suscipit', 5, 5);
+insert into Feedbacks (Feedback, CustomerId, BarbersId) values ('consequat in consequat ut nulla sed accumsan felis ut at', 6, 6);
+insert into Feedbacks (Feedback, CustomerId, BarbersId) values ('erat tortor sollicitudin mi sit amet lobortis sapien sapien non', 7, 7);
+insert into Feedbacks (Feedback, CustomerId, BarbersId) values ('euismod scelerisque quam turpis adipiscing lorem vitae mattis nibh', 8, 8);
+insert into Feedbacks (Feedback, CustomerId, BarbersId) values ('luctus et ultrices posuere cubilia curae duis faucibus accumsan', 9, 9);
+insert into Feedbacks (Feedback, CustomerId, BarbersId) values ('donec semper sapien a libero nam', 10, 10);
 
-insert into GroupsLectures (GroupId, LectureId) values (5, 15);
-insert into GroupsLectures (GroupId, LectureId) values (16, 13);
-insert into GroupsLectures (GroupId, LectureId) values (17, 15);
-insert into GroupsLectures (GroupId, LectureId) values (9, 14);
-insert into GroupsLectures (GroupId, LectureId) values (19, 13);
-insert into GroupsLectures (GroupId, LectureId) values (9, 12);
-insert into GroupsLectures (GroupId, LectureId) values (19, 12);
-insert into GroupsLectures (GroupId, LectureId) values (3, 14);
-insert into GroupsLectures (GroupId, LectureId) values (15, 8);
-insert into GroupsLectures (GroupId, LectureId) values (17, 15);
-insert into GroupsLectures (GroupId, LectureId) values (5, 13);
-insert into GroupsLectures (GroupId, LectureId) values (5, 1);
-insert into GroupsLectures (GroupId, LectureId) values (2, 4);
-insert into GroupsLectures (GroupId, LectureId) values (16, 1);
-insert into GroupsLectures (GroupId, LectureId) values (11, 10);
-insert into GroupsLectures (GroupId, LectureId) values (9, 10);
-insert into GroupsLectures (GroupId, LectureId) values (7, 2);
-insert into GroupsLectures (GroupId, LectureId) values (18, 11);
-insert into GroupsLectures (GroupId, LectureId) values (13, 1);
-insert into GroupsLectures (GroupId, LectureId) values (19, 7);
-go
+insert into EstimationTypes ([Type]) values ('magna');
+insert into EstimationTypes ([Type]) values ('sit');
+insert into EstimationTypes ([Type]) values ('in');
+insert into EstimationTypes ([Type]) values ('fusce');
+insert into EstimationTypes ([Type]) values ('orci');
+insert into EstimationTypes ([Type]) values ('vestibulum');
+insert into EstimationTypes ([Type]) values ('ipsum');
+insert into EstimationTypes ([Type]) values ('nullam');
+insert into EstimationTypes ([Type]) values ('consequat');
+insert into EstimationTypes ([Type]) values ('sit');
 
-insert into GroupsCurators
-values (1, 3),
-       (4, 5),
-	   (3, 4),
-	   (5, 8),
-	   (2, 2),
-	   (6, 7),
-	   (7, 1),
-	   (1, 9),
-	   (7, 6),
-	   (3, 10),
-	   (7, 13),
-	   (2, 18),
-	   (1, 14),
-	   (3, 19),
-	   (2, 11)
-go
+insert into Estimations (CustomerId, BarbersId, TypeId) values (1, 1, 1);
+insert into Estimations (CustomerId, BarbersId, TypeId) values (2, 2, 2);
+insert into Estimations (CustomerId, BarbersId, TypeId) values (3, 3, 3);
+insert into Estimations (CustomerId, BarbersId, TypeId) values (4, 4, 4);
+insert into Estimations (CustomerId, BarbersId, TypeId) values (5, 5, 5);
+insert into Estimations (CustomerId, BarbersId, TypeId) values (6, 6, 6);
+insert into Estimations (CustomerId, BarbersId, TypeId) values (7, 7, 7);
+insert into Estimations (CustomerId, BarbersId, TypeId) values (8, 8, 8);
+insert into Estimations (CustomerId, BarbersId, TypeId) values (9, 9, 9);
+insert into Estimations (CustomerId, BarbersId, TypeId) values (10, 10, 10);
+
+insert into BarbersServices (ServiceId, BarbersId) values (1, 1);
+insert into BarbersServices (ServiceId, BarbersId) values (2, 2);
+insert into BarbersServices (ServiceId, BarbersId) values (3, 3);
+insert into BarbersServices (ServiceId, BarbersId) values (4, 4);
+insert into BarbersServices (ServiceId, BarbersId) values (5, 5);
+insert into BarbersServices (ServiceId, BarbersId) values (6, 6);
+insert into BarbersServices (ServiceId, BarbersId) values (7, 7);
+insert into BarbersServices (ServiceId, BarbersId) values (8, 8);
+insert into BarbersServices (ServiceId, BarbersId) values (9, 9);
+insert into BarbersServices (ServiceId, BarbersId) values (10, 10);
+
+insert into Archive (CustomerId, BarbersId, ServiceId, [Date], EstimationId, FeedbackId, TotalPrice) values (1, 1, 1, '1/25/2022', 1, 1, '$7.30');
+insert into Archive (CustomerId, BarbersId, ServiceId, [Date], EstimationId, FeedbackId, TotalPrice) values (2, 2, 2, '12/3/2022', 2, 2, '$5.63');
+insert into Archive (CustomerId, BarbersId, ServiceId, [Date], EstimationId, FeedbackId, TotalPrice) values (3, 3, 3, '2/24/2022', 3, 3, '$9.24');
+insert into Archive (CustomerId, BarbersId, ServiceId, [Date], EstimationId, FeedbackId, TotalPrice) values (4, 4, 4, '1/5/2022', 4, 4, '$8.00');
+insert into Archive (CustomerId, BarbersId, ServiceId, [Date], EstimationId, FeedbackId, TotalPrice) values (5, 5, 5, '10/21/2022', 5, 5, '$4.96');
+insert into Archive (CustomerId, BarbersId, ServiceId, [Date], EstimationId, FeedbackId, TotalPrice) values (6, 6, 6, '3/5/2022', 6, 6, '$4.52');
+insert into Archive (CustomerId, BarbersId, ServiceId, [Date], EstimationId, FeedbackId, TotalPrice) values (7, 7, 7, '2/24/2022', 7, 7, '$3.05');
+insert into Archive (CustomerId, BarbersId, ServiceId, [Date], EstimationId, FeedbackId, TotalPrice) values (8, 8, 8, '8/18/2022', 8, 8, '$9.01');
+insert into Archive (CustomerId, BarbersId, ServiceId, [Date], EstimationId, FeedbackId, TotalPrice) values (9, 9, 9, '10/5/2022', 9, 9, '$2.75');
+insert into Archive (CustomerId, BarbersId, ServiceId, [Date], EstimationId, FeedbackId, TotalPrice) values (10, 10, 10, '3/28/2022', 10, 10, '$8.24');
